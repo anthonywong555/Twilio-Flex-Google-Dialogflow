72bf3371f994509c26b88139455cb5ea7aa4d56f
Anthony Wong <anthonywong555@gmail.com>
2020-06-18 13:08:53 -0400

    Add loadGoogleCreds, sendToTranslate, and formattResponse

--- twilio/serverless/functions/google/translate/detectLanguage.js
+++ twilio/serverless/functions/google/translate/detectLanguage.js
@@ -30,8 +30,54 @@ const loadServerlessModules = async () => {
 
 const driver = async(serverlessContext, serverlessEvent, twilioClient) => {
   try {
-
+    const googleCreds = loadGoogleCreds();
+    const response = await sendToTranslate(serverlessContext, serverlessEvent, googleCreds);
+    const result = formatGoogleResponse(response);
+    return result;
   } catch (e) {
     throw serverlessHelper.formatErrorMsg(serverlessContext, 'driver', e);
+  }
+}
+
+const loadGoogleCreds = () => {
+  try {
+    const googleCreds = JSON.parse(
+      Runtime.getAssets()['/google-dialogflow-service-account-key.json'].open()
+    );
+    return googleCreds;
+  } catch (e) {
+    throw serverlessHelper.formatErrorMsg(serverlessContext, 'loadGoogleCreds', e);
+  }
+}
+
+const sendToTranslate = async (serverlessContext, serverlessEvent, googleCreds) => {
+  try {
+    const translationClient = new TranslationServiceClient(googleCreds);
+    const request = {
+      parent: `projects/${projectId}/locations/${location}`,
+      content: text,
+    };
+
+    const [response] = await translationClient.detectLanguage(request);
+
+    console.log('Detected Languages:');
+    for (const language of response.languages) {
+      console.log(`Language Code: ${language.languageCode}`);
+      console.log(`Confidence: ${language.confidence}`);
+    }
+    
+    return response;
+
+  } catch (e) {
+    throw serverlessHelper.formatErrorMsg(serverlessContext, 'sendToDialogFlow', e);
+  }
+}
+
+const formatGoogleResponse = (serverlessContext, googleResponse) => {
+  try {
+    const result = {};
+    return googleResponse
+  } catch (e) {
+    throw serverlessHelper.formatErrorMsg(serverlessContext, 'formatGoogleResponse', e);
   }
 }
